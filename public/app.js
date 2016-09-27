88d43bd18aa8d67912dcb9b7053564f8af5567a7
siansrd <sianrobinsondavies@gmail.com>
2016-09-27 20:55:28 +0100

    Add button to change location.

--- public/app.js
+++ public/app.js
@@ -1,9 +1,20 @@
+
+   
+
 var initialize = function() {
   var container = document.getElementById('map');
-  var center = { lat: 55.94716, lng: -3.20198 }
+  var center = { lat: 55.94716, lng: -3.20198 };
   var map = new Map(container, center, 10);
   map.addMarker(center);
   // map.addClickEvent();
+
+  // Move Locations button
+  moveOnClick = function() {
+    var newCenter = { lat: 52.406822, lng: -1.519693 };
+    map.panTo(newCenter);
+  }
+  var button = document.querySelector('button');
+  button.onclick = moveOnClick();
 }
 
 window.onload = initialize;
--- public/marker.js
+++ public/marker.js
@@ -1,8 +1,16 @@
 var Marker = function(coords, map) {
 
-  this.marker = new google.maps.Marker({
-    position: coords,
-    map: map
+  this.marker = new google.maps.Marker({ 
+    position: coords, 
+    map: map 
   });
+
+}
+
+Marker.prototype = {
+
+  // addListener: function(click, function() {
+  //   map.setZoom(8);
+  // });
 
 }
